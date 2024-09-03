419fd16cfebf167c424ba1d6e137afe797c6ad0f
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-03 16:17:28 -0600

    updated car model

--- app/models/Car.js
+++ app/models/Car.js
@@ -32,17 +32,14 @@ export class Car {
           <div class="p-2">
             <p class="fs-3">
             ${this.year} ${this.make} ${this.model}
-            <i onclick="app.CarsController.deleteCar('${this.id}')" class="mdi mdi-delete-forever text-danger" role="button" title="Delete this ${this.make} ${this.model}"></i>
             </p>
-            <p>${this.description}</p>
-
             <p class="fs-4">
             Listed on ${this.listedDate} at ${this.listedTime} 
            </p>
            <p class="fs-4">$${this.priceAsCurrency}</p>
            <p>${this.description}</p>
            <p class="fs-3">
-             ${this.transmissionIcon}
+             ${this.engineType}
            </p>
             <div class="d-flex justify-content-end align-items-center">
               <span>${this.creator.name}</span>
@@ -61,17 +58,6 @@ export class Car {
   get listedTime() {
     return this.createdAt.toLocaleTimeString()
   }
-
-  get transmissionIcon() {
-    if (this.engineType == 'manual') {
-      return '<i class="mdi mdi-car-shift-pattern" title="Manual Transmission"></i>'
-    }
-    if (this.engineType == 'automatic') {
-      return '<i class="mdi mdi-refresh-auto" title="Automatic Transmission"></i>'
-    }
-    return '<i class="mdi mdi-help" title="Unknown Transmission"></i>'
-  }
-
   get priceAsCurrency() {
     return new Intl.NumberFormat().format(this.price)
   }
