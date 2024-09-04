ac01d6948bb6e60a096bc7d262e7e40d73789585
James54509 <jamesc@newvisualmedia.com>
2024-09-03 18:43:24 -0600

    jobs class created, moving onto form data for houses page

---
+++ app/models/Job.js
@@ -0,0 +1,18 @@
+export class Job{
+constructor(data){
+this.company = data.company
+this.jobTitle = data.jobTitle
+this.hours = data.hours
+this.rate = data.rate
+this.description = data.description
+this.creatorId = data.creatorId
+
+
+}
+
+
+
+}
+
+
+
