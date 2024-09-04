f0a8890f9ab64fab16d46f46ebc857f5b81845e9
James54509 <jamesc@newvisualmedia.com>
2024-09-03 19:48:55 -0600

    form is hidden without user login

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -1,5 +1,6 @@
 import { AppState } from "../AppState.js";
 import { housesService } from "../services/HousesService.js";
+import { getFormData } from "../utils/FormHandler.js";
 import { Pop } from "../utils/Pop.js";
 import { setHTML } from "../utils/Writer.js";
 export class HousesController {
@@ -19,6 +20,19 @@ export class HousesController {
     setHTML('houses-listings', houseHTML)
   }
 
+
+  showHouseForm(){
+    if (AppState.user == null) {return}
+
+    const houseFormElem = document.getElementById('house-form')
+
+    if (houseFormElem == null) {return}
+
+    houseFormElem.classList.remove('d-none')
+  }
+
+
+
   async getHouses() {
     try {
       await housesService.getHouses()
@@ -26,8 +40,26 @@ export class HousesController {
       Pop.error(error)
       console.error(error)
     }
-
-
-
   }
+
+
+  async createHouse(){
+    try{
+      event.preventDefault()
+      const houseFormElem = event.target
+      const houseFormData = getFormData(houseFormElem)
+      await housesService.createHouse(houseFormData)
+      Pop.toast('you created the house listing!')
+
+      // @ts-ignore
+      houseFormElem.reset() 
+    }catch (error) {
+      Pop.error(error)
+      console.log(error)
+    }
+  }
+
+
+
 }
+
